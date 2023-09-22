03d92dc19ed7c30f89ad23a64f198ea020514be9
Himanshu <hv65616@gmail.com>
2023-09-23 03:54:07 +0530

    db connection

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ dotenv.config({ path: "./config.env" });
 const app = require("./backend/app");
 const mongoose = require("mongoose");
 const port = process.env.PORT || 3000;
+const db = process.env.DATABASE.replace(
+  "<password>",
+  process.env.DATABASE_PASSWORD
+);
 
 process.on("uncaughtException", (err) => {
   console.log(err.name, err.message);
@@ -11,6 +15,15 @@ process.on("uncaughtException", (err) => {
     process.exit(1);
   });
 });
+
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((con) => {
+    console.log("Database connection successful");
+  });
 
 const server = app.listen(port, () => {
   console.log("Server is listening on port 3000");
